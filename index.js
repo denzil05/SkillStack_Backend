f1a2d916b901b3bee5f14f28b5736d0d901cb16b
Denzil Martin.J <denzilmart@gmail.com>
2025-01-22 13:32:48 +0100

    making it use port from render

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors'); // Import cors
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // Enable CORS for all requests
 app.use(cors());
